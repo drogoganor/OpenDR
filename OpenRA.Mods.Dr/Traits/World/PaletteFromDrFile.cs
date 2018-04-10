8cd2ece039a7c9253fb0a193fc4c3512800f0cad
David Wilson <spamdog@gmail.com>
2018-04-10 23:23:12 +1000

    Added PaletteFromDrFile to use original terrain palette files. Updated installers to match. Removed defunct jasc palettes.

---
+++ OpenRA.Mods.Dr/Traits/World/PaletteFromDrFile.cs
@@ -0,0 +1,127 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Dr.Traits
+{
+	[Desc("Load a Dark Reign .PAL palette file. Index 0 is hardcoded to be fully transparent/invisible.")]
+	class PaletteFromDrFileInfo : ITraitInfo
+	{
+		[FieldLoader.Require, PaletteDefinition]
+		[Desc("Palette name used internally.")]
+		public readonly string Name = null;
+
+		[Desc("Defines for which tileset IDs this palette should be loaded.",
+			"If none specified, it applies to all tileset IDs not explicitly excluded.")]
+		public readonly HashSet<string> Tilesets = new HashSet<string>();
+
+		[Desc("Don't load palette for these tileset IDs.")]
+		public readonly HashSet<string> ExcludeTilesets = new HashSet<string>();
+
+		[FieldLoader.Require]
+		[Desc("Name of the file to load.")]
+		public readonly string Filename = null;
+
+		[Desc("Premultiply colors with their alpha values.")]
+		public readonly bool Premultiply = true;
+
+		public readonly bool AllowModifiers = true;
+
+		public object Create(ActorInitializer init) { return new PaletteFromDrFile(init.World, this); }
+	}
+
+	class PaletteFromDrFile : ILoadsPalettes, IProvidesAssetBrowserPalettes
+	{
+		readonly World world;
+		readonly PaletteFromDrFileInfo info;
+
+		public PaletteFromDrFile(World world, PaletteFromDrFileInfo info)
+		{
+			this.world = world;
+			this.info = info;
+		}
+
+		public void LoadPalettes(WorldRenderer wr)
+		{
+			var colors = new uint[Palette.Size];
+			using (var s = world.Map.Open(info.Filename))
+			{
+				var headerName = s.ReadASCII(4);
+				var headerVersion = s.ReadInt32();
+
+				if (headerName != "PALS")
+				{
+					throw new InvalidDataException("Palette header was not PALS");
+				}
+
+				if (headerVersion != 0x0102)
+				{
+					throw new InvalidDataException("Palette version `{0}` was incorrect (expected `0x0102`)".F(headerVersion));
+				}
+
+				// Data is made up of 3x256 bytes, each ranging 0-63. Data is grouped by channel.
+				var list = new List<byte>();
+				for (int i = 0; i < Palette.Size * 6; i++)
+				{
+					list.Add(s.ReadUInt8());
+				}
+
+				//list = list.Skip(768).Take(768).ToList(); // Get the second palette, ranging 0-31
+
+				var rList = list.Take(256).ToList();
+				var gList = list.Skip(256).Take(256).ToList();
+				var bList = list.Skip(512).Take(256).ToList();
+				
+				int standardMultiplier = 4;
+				int terrainMultiplier = 4; // 6: jungle, barren   5 possibly: aust, auralien, asteroid   4: all others
+
+				// Nasty hack to brighten dark palettes
+				var lowerFilename = info.Filename.ToLowerInvariant();
+				if (lowerFilename.Contains("jungle") || lowerFilename.Contains("barren")) // Or asteroid
+					terrainMultiplier = 6;
+				else if (lowerFilename.Contains("aust") || lowerFilename.Contains("auralien") || lowerFilename.Contains("asteroid"))
+					terrainMultiplier = 5;
+
+				for (int i = 0; i < Palette.Size; i++)
+				{
+					// Index 0 should always be completely transparent/background color
+					if (i == 0)
+						colors[i] = 0;
+					else if (i < 160 || i == 255)
+						colors[i] = (uint)Color.FromArgb(rList[i] * standardMultiplier, gList[i] * standardMultiplier, bList[i] * standardMultiplier).ToArgb();
+					else
+						colors[i] = (uint)Color.FromArgb(rList[i] * terrainMultiplier, gList[i] * terrainMultiplier, bList[i] * terrainMultiplier).ToArgb();
+				}
+			}
+
+			wr.AddPalette(info.Name, new ImmutablePalette(colors), info.AllowModifiers);
+		}
+
+		public IEnumerable<string> PaletteNames
+		{
+			get
+			{
+				// Only expose the palette if it is available for the shellmap's tileset (which is a requirement for its use).
+				if ((info.Tilesets.Count == 0 || info.Tilesets.Contains(world.Map.Rules.TileSet.Id))
+					&& !info.ExcludeTilesets.Contains(world.Map.Rules.TileSet.Id))
+					yield return info.Name;
+			}
+		}
+	}
+}
