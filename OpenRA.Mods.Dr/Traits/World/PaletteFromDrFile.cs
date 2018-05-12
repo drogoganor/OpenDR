f64f8179868321489205a1144b1839deaab92f93
David Wilson <spamdog@gmail.com>
2018-05-12 14:56:03 +1000

    Added optional file loading for palette file

--- OpenRA.Mods.Dr/Traits/World/PaletteFromDrFile.cs
+++ OpenRA.Mods.Dr/Traits/World/PaletteFromDrFile.cs
@@ -53,61 +53,66 @@ namespace OpenRA.Mods.Dr.Traits
 
 		public void LoadPalettes(WorldRenderer wr)
 		{
-			//wr.AddPalette(info.Name, new ImmutablePalette(world.Map.Open(info.Filename), info.ShadowIndex), info.AllowModifiers);
-
 			var colors = new uint[Palette.Size];
-			using (var s = world.Map.Open(info.Filename))
+			Stream s;
+			if (!world.Map.TryOpen(info.Filename, out s))
 			{
-				var headerName = s.ReadASCII(4);
-				var headerVersion = s.ReadInt32();
+				//throw new FileNotFoundException("Couldn't find palette file: " + info.Filename);
+				return;
+			}
+			
+			var headerName = s.ReadASCII(4);
+			var headerVersion = s.ReadInt32();
 
-				if (headerName != "PALS")
-				{
-					throw new InvalidDataException("Palette header was not PALS");
-				}
+			if (headerName != "PALS")
+			{
+				throw new InvalidDataException("Palette header was not PALS");
+			}
 
-				if (headerVersion != 0x0102)
-				{
-					throw new InvalidDataException("Palette version `{0}` was incorrect (expected `0x0102`)".F(headerVersion));
-				}
+			if (headerVersion != 0x0102)
+			{
+				throw new InvalidDataException("Palette version `{0}` was incorrect (expected `0x0102`)".F(headerVersion));
+			}
 
-				// Data is made up of 3x256 bytes, each ranging 0-63. Data is grouped by channel.
-				var list = new List<byte>();
-				for (int i = 0; i < Palette.Size * 6; i++)
-				{
-					list.Add(s.ReadUInt8());
-				}
+			// Data is made up of 3x256 bytes, each ranging 0-63. Data is grouped by channel.
+			var list = new List<byte>();
+			for (int i = 0; i < Palette.Size * 6; i++)
+			{
+				list.Add(s.ReadUInt8());
+			}
 
-				//list = list.Skip(768).Take(768).ToList(); // Get the second palette, ranging 0-31
+			//list = list.Skip(768).Take(768).ToList(); // Get the second palette, ranging 0-31
 
-				var rList = list.Take(256).ToList();
-				var gList = list.Skip(256).Take(256).ToList();
-				var bList = list.Skip(512).Take(256).ToList();
+			var rList = list.Take(256).ToList();
+			var gList = list.Skip(256).Take(256).ToList();
+			var bList = list.Skip(512).Take(256).ToList();
 
-				int standardMultiplier = 4;
-				int terrainMultiplier = 4; // 6: jungle, barren   5 possibly: aust, auralien, asteroid   4: all others
+			int standardMultiplier = 4;
+			int terrainMultiplier = 4; // 6: jungle, barren   5 possibly: aust, auralien, asteroid   4: all others
 
-				// Nasty hack to brighten dark palettes
-				var lowerFilename = info.Filename.ToLowerInvariant();
-				if (lowerFilename.Contains("jungle") || lowerFilename.Contains("barren")) // Or asteroid
-					terrainMultiplier = 6;
-				else if (lowerFilename.Contains("aust") || lowerFilename.Contains("auralien") || lowerFilename.Contains("asteroid"))
-					terrainMultiplier = 5;
+			// Nasty hack to brighten dark palettes
+			var lowerFilename = info.Filename.ToLowerInvariant();
+			if (lowerFilename.Contains("jungle") || lowerFilename.Contains("barren")) // Or asteroid
+				terrainMultiplier = 6;
+			else if (lowerFilename.Contains("aust") || lowerFilename.Contains("auralien") || lowerFilename.Contains("asteroid"))
+				terrainMultiplier = 5;
 
-				for (int i = 0; i < Palette.Size; i++)
-				{
-					// Index 0 should always be completely transparent/background color
-					if (i == 0)
-						colors[i] = 0;
-					else if (i < 160 || i == 255)
-						colors[i] = (uint)Color.FromArgb(rList[i] * standardMultiplier, gList[i] * standardMultiplier, bList[i] * standardMultiplier).ToArgb();
-					else
-						colors[i] = (uint)Color.FromArgb(rList[i] * terrainMultiplier, gList[i] * terrainMultiplier, bList[i] * terrainMultiplier).ToArgb();
-				}
+			for (int i = 0; i < Palette.Size; i++)
+			{
+				// Index 0 should always be completely transparent/background color
+				if (i == 0)
+					colors[i] = 0;
+				else if (i < 160 || i == 255)
+					colors[i] = (uint)Color.FromArgb(rList[i] * standardMultiplier, gList[i] * standardMultiplier, bList[i] * standardMultiplier).ToArgb();
+				else
+					colors[i] = (uint)Color.FromArgb(rList[i] * terrainMultiplier, gList[i] * terrainMultiplier, bList[i] * terrainMultiplier).ToArgb();
 			}
 
 			if (info.Tileset == null || info.Tileset.ToLowerInvariant() == world.Map.Tileset.ToLowerInvariant())
 				wr.AddPalette(info.Name, new ImmutablePalette(colors), info.AllowModifiers);
+
+			s.Close();
+			s.Dispose();
 		}
 
 		public IEnumerable<string> PaletteNames
