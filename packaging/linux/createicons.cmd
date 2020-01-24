6eca454d9516eb09229d58c68cac0288ab4d32b9
David Wilson <spamdog@gmail.com>
2020-01-24 16:12:20 +1000

    Updated windows icon

--- packaging/linux/createicons.cmd
+++ packaging/linux/createicons.cmd
@@ -1,10 +1,10 @@
 
-"C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -define icon:auto-resize mod.ico
-"C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 16x16 mod_16x16.png
-"C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 32x32 mod_32x32.png
-"C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 48x48 mod_48x48.png
-"C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 64x64 mod_64x64.png
-"C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 128x128 mod_128x128.png
-"C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 256x256 mod_256x256.png
+"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -define icon:auto-resize mod.ico
+"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 16x16 mod_16x16.png
+"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 32x32 mod_32x32.png
+"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 48x48 mod_48x48.png
+"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 64x64 mod_64x64.png
+"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 128x128 mod_128x128.png
+"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe" convert -transparent white -density 384 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 256x256 mod_256x256.png
 
-"C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe" convert -transparent white -density 1024 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 252x252 banner_252x252.png -transparent white
+"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe" convert -transparent white -density 1024 "D:\Development\DarkReign\packaging\linux\mod_scalable.svg" -resize 252x252 banner_252x252.png -transparent white
