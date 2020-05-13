dbdc03d6d1098c93de9211a640934561eda898db
David Wilson <spamdog@gmail.com>
2020-05-13 20:43:49 +1000

    Commit of current work refactoring chrome

--- packaging/source/createico.ps1
+++ packaging/source/createico.ps1
@@ -12,6 +12,7 @@ Invoke-Expression "$imgMagick convert -transparent white -background None -densi
 Invoke-Expression "$imgMagick convert -transparent white -background None -density 384 $image -resize 128x128 mod_128x128.png"
 Invoke-Expression "$imgMagick convert -transparent white -background None -density 384 $image -resize 256x256 mod_256x256.png"
 Invoke-Expression "$imgMagick convert -transparent white -background None -density 384 $image -resize 512x512 mod_512x512.png"
+Invoke-Expression "$imgMagick convert -transparent white -background None -density 384 $image -resize 768x768 mod_768x768.png"
 Invoke-Expression "$imgMagick convert -transparent white -background None -density 384 $image -resize 1024x1024 mod_1024x1024.png"
 Invoke-Expression "$imgMagick convert -transparent white -background None -density 1024 $image -resize 252x252 banner_252x252.png"
 Invoke-Expression "$imgMagick convert -transparent white -background None -density 384 $image -resize 1280x640 github_socialmedia_1280x640.png"
